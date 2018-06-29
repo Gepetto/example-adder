35328fb6bcaded277b185dc87f38bf4bd27c2cb8
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-06-29 18:23:40 +0200

    [Adder] Create Library

---
+++ include/example-adder/gepadd.hpp
@@ -0,0 +1,13 @@
+#ifndef __gep_add__
+#define __gep_add__
+
+namespace gepetto
+{
+  namespace example
+  {
+    int add(const int a, const int b);
+    int sub(const int a, const int b);
+  }
+}
+
+#endif
---
+++ src/gepadd.cpp
@@ -0,0 +1,17 @@
+#include "example-adder/gepadd.hpp"
+
+namespace gepetto
+{
+  namespace example
+  {
+    int add(const int a, const int b)
+    {
+      return a + b;
+    }
+
+    int sub(const int a, const int b)
+    {
+      return a - b;
+    }
+  }
+}
