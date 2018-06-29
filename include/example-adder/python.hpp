006175d07964d0f6cf96d6de880821124d824346
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-06-29 19:01:52 +0200

    Bindings: Add Python bindings

---
+++ include/example-adder/python.hpp
@@ -0,0 +1,14 @@
+#ifndef EXAMPLE_ADDER_PYTHON_HPP
+#define EXAMPLE_ADDER_PYTHON_HPP
+
+#include <boost/python.hpp>
+
+#include "example-adder/gepadd.hpp"
+
+namespace gepetto {
+namespace example {
+void exposeExampleAdder();
+} // namespace example
+} // namespace gepetto
+
+#endif
