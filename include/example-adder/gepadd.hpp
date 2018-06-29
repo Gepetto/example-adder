2cf411059642dc1dafc447698990a41525427ecc
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-06-29 18:23:40 +0200

    [Adder] Create Library

---
+++ include/example-adder/gepadd.hpp
@@ -0,0 +1,11 @@
+#ifndef __gep_add__
+#define __gep_add__
+
+namespace gepetto {
+namespace example {
+long add(const long a, const long b);
+long sub(const long a, const long b);
+}  // namespace example
+}  // namespace gepetto
+
+#endif
