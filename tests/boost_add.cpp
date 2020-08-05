927807721a05ef783507ff860ec9faa8595fc0b0
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-08-05 17:08:04 +0200

    wrong maths

--- tests/boost_add.cpp
+++ tests/boost_add.cpp
@@ -5,7 +5,7 @@
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
 BOOST_AUTO_TEST_CASE(test_boost_add) {
-  BOOST_CHECK(gepetto::example::add(1, 2) == 3);
+  BOOST_CHECK(gepetto::example::add(1, 2) == 4);
   BOOST_CHECK(gepetto::example::add(5, -1) == 4);
   BOOST_CHECK(gepetto::example::add(-3, 1) == -2);
 }
